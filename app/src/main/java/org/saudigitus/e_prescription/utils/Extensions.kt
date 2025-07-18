dd5cd2e3bdd12f25d6ff0f1c1e481b25c6accee7
CarlosMacaneta <carlosmacaneta95@gmail.com>
2025-07-18 09:48:00 +0200

    chore: cleaning code

--- app/src/main/java/org/saudigitus/e_prescription/utils/Extensions.kt
+++ app/src/main/java/org/saudigitus/e_prescription/utils/Extensions.kt
@@ -1,10 +1,7 @@
 package org.saudigitus.e_prescription.utils
 
-import org.saudigitus.e_prescription.data.model.MedicineIndicators
 import org.saudigitus.e_prescription.data.model.Prescription
 import org.saudigitus.e_prescription.data.model.PrescriptionError
-import org.saudigitus.e_prescription.data.model.put.DataValue
-import org.saudigitus.e_prescription.data.model.put.UpdateEvent
 import org.saudigitus.e_prescription.data.model.response.Attribute
 
 
@@ -16,39 +13,9 @@ fun Prescription.toPrescriptionError(givenQtd: Int) =
         givenQtd = givenQtd
     )
 
-/**
- * returns indicators elements as list
- * Pair(string: label, int: count)
- */
-fun MedicineIndicators.toList() =
-    listOf(
-        this.completed,
-        this.incomplete,
-        this.zero
-    )
-
 fun List<Attribute>.getByAttr(attr: String): Pair<String, String> {
     val attr = find { it.attribute == attr }
 
     return Pair(attr?.displayName.orEmpty(), attr?.value.orEmpty())
 }
 
-fun Prescription.toUpdateEvent(
-    trackedEntityInstance: String,
-    dataElement: String,
-    value: Int
-) =
-    UpdateEvent(
-        event = this.uid,
-        orgUnit = this.ou,
-        dataValues = listOf(
-            DataValue(
-                dataElement = dataElement,
-                value = value
-            )
-        ),
-        program = UIDMapping.PROGRAM,
-        programStage = UIDMapping.PROGRAM_STAGE,
-        status = "ACTIVE",
-        trackedEntityInstance = trackedEntityInstance,
-    )
